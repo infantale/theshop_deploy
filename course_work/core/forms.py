0d6a3db8f8e9e1324a5c1a39b024303f5364e0e7
infantale <71599070+infantale@users.noreply.github.com>
2021-05-03 12:50:47 +0300

    register mechanism done

--- course_work/core/forms.py
+++ course_work/core/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
 
 from .models import AdvUser
 
@@ -6,3 +8,39 @@ class ChangeUserInfoForm(forms.ModelForm):
     class Meta:
         model = AdvUser
         fields = ('username', 'first_name', 'last_name')
+
+
+class RegisterUserFrom(forms.ModelForm):
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    def clean_password1(self):
+        password1 = self.cleaned_data['password1']
+        if password1:
+            password_validation.validate_password(password1)
+        return password1
+
+    def clean(self):
+        super().clean()
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            errors = {'password2': ValidationError('Введённые пароли не совпадают', \
+                        code='password_mismatch')}
+            raise ValidationError(errors)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = True
+        user.is_activated = True
+
+        if commit:
+            user.save()
+
+        return user
+
+    class Meta:
+        model = AdvUser
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
--- course_work/core/urls.py
+++ course_work/core/urls.py
@@ -4,7 +4,10 @@ from .views import *
 
 app_name = 'core'
 urlpatterns = [
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('accounts/logout/', TSLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('login/', TSLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
