078feeb28b223e66790adbeac91d79025911f8f1
infantale <71599070+infantale@users.noreply.github.com>
2021-05-11 17:49:44 +0300

    djangorestframework
    2 urls

---
+++ course_work/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView
+
+from core.models import Bb
+from .serializers import *
+
+
+@api_view(['GET'])
+def bbs(request):
+    if request.method == 'GET':
+        bbs = Bb.objects.filter(is_active=True)[:10]
+        serializer = BbSerializer(bbs, many=True)
+        return Response(serializer.data)
+
+
+class BbDetailView(RetrieveAPIView):
+    queryset = Bb.objects.filter(is_active=True)
+    serializer_class = BbDetailSerializer
