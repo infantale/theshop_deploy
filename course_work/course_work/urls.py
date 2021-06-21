06629f16f70177b389ab1400d94fa83d9ef36bf0
infantale <71599070+infantale@users.noreply.github.com>
2021-06-21 10:47:22 +0300

    heroku

--- course_work/course_work/urls.py
+++ course_work/course_work/urls.py
@@ -20,15 +20,16 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.views import serve
 from django.views.decorators.cache import never_cache
 
-# Регистрируем api
-# apipatterns = [
-#     url(r'^', include('api.urls'))
-# ]
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('core.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
---
+++ course_work/procfile
@@ -0,0 +1,1 @@
+web: gunicorn course_work.wsgi --log-file -
