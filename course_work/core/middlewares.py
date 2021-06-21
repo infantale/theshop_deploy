6c4311066d9ca6018a7ce5ccf180354a95411f62
infantale <71599070+infantale@users.noreply.github.com>
2021-06-21 11:12:03 +0300

    Delete course_work directory

--- course_work/core/middlewares.py
+++
@@ -1,7 +0,0 @@
-from .models import SubCategory, SuperCategory
-
-def tshop_context_processor(request):
-    context = {}
-    context['super_categories'] = SuperCategory.objects.all()
-    context['sub_categories'] = SubCategory.objects.all()
-    return context
--- course_work/core/utilities.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-from os.path import splitext
-
-def get_timestamp_path(instance, filename):
-    return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
